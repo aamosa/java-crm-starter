cbc34abeae3a0a3ff3a0901d7a8b5ea700335854
aamosa <44481508+aamosa@users.noreply.github.com>
2020-04-11 17:58:22 -0500

    Starting Primefaces

---
+++ src/main/java/com/customer/syn/util/ValueLabelHolder.java
@@ -0,0 +1,60 @@
+package com.customer.syn.util;
+
+public class ValueLabelHolder<T> {
+    
+    private String label;
+    private T value;
+    
+    public ValueLabelHolder(String label, T value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((label == null) ? 0 : label.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ValueLabelHolder<?> other = (ValueLabelHolder<?>) obj;
+        if (label == null) {
+            if (other.label != null)
+                return false;
+        } else if (!label.equals(other.label))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
+}
