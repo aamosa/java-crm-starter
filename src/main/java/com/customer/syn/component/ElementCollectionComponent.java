60acb743d8ed6b3ec83d0ef0b28fa57c4f4a4236
aamosa <44481508+aamosa@users.noreply.github.com>
2020-12-25 14:39:16 -0600

    development wip

--- src/main/java/com/customer/syn/component/ElementCollectionComponent.java
+++ src/main/java/com/customer/syn/component/ElementCollectionComponent.java
@@ -1,5 +1,8 @@
 package com.customer.syn.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.faces.component.FacesComponent;
 import javax.faces.component.NamingContainer;
 import javax.faces.component.UIInput;
@@ -9,6 +12,22 @@ import javax.faces.component.UINamingContainer;
 @FacesComponent(value = "elementcollection")
 public class ElementCollectionComponent extends UIInput
     implements NamingContainer {
+
+    private static final Logger log = LoggerFactory.getLogger(ElementCollectionComponent.class);
+
+    private Object childClass;
+
+
+    public Object getChildClass()
+    throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class<?> refType = (Class<?>) getAttributes().get("referencedType");
+        Class<?> c = Class.forName(refType.getName());
+        log.debug("class = {}", c.getName());
+        return c.newInstance();
+    }
+
+
+
 
 
     @Override
